88b6e730a9152d44ff7f89d275891efa969d680b
Sam Mussell <sam@streetsaheadllc.com>
2011-02-15 07:56:00 -0700

    added mit liscence headers

--- client-scripts/formdefault.jquery.js
+++ client-scripts/formdefault.jquery.js
@@ -1,9 +1,30 @@
-/**
+/*
+ * Copyright (C) 2011 by Streets Ahead LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *
+ * 
  * A little jQuery plugin to handle inner labels for forms.
  *
  * Written by Streets Ahead LLC. http://streetsaheadllc.com
- * You are free to use this plugin source if it helps you, 
- * just please give us credit if you use it in full.
+ * 
  */ 
 
 (function( $ ){
